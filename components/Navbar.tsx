3c5687ba0ffaa5fab007f8f265dd9f627c784c26
Pratham Sitoula <psitoula2003@gmail.com>
2022-08-16 15:40:36 -0600

    Login functionality implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,11 @@ import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-  const user = false;
+  const { userProfile, addUser } = useAuthStore();
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href={"/"}>
@@ -27,10 +29,10 @@ const Navbar = () => {
       <div>SEARCH</div>
 
       <div>
-        {user && <div> Logged In</div>}
-        {!user && (
+        {userProfile && <div> {userProfile.userName}</div>}
+        {!userProfile && (
           <GoogleLogin
-            onSuccess={(response) => createOrGetUser(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log("error")}
           />
         )}
