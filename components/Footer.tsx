750424f20c1af49e348b3d0a8fbcb1f49fda350b
Pratham Sitoula <psitoula2003@gmail.com>
2022-08-15 18:06:48 -0600

    Footer setup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { footerList1, footerList2, footerList3 } from "../utils/constants";
+import List from "./List";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt={true} />
+      <List items={footerList3} mt={true} />
+      <p className="text-gray-400 text-sm mt-5">2022 Tiktik</p>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ components/List.tsx
@@ -0,0 +1,16 @@
+const List = ({ items, mt }: { items: string[]; mt: boolean }): JSX.Element => {
+  return (
+    <div className={`flex flex-wrap gap-2 ${mt ? "mt-5" : ""}`}>
+      {items.map((item) => (
+        <p
+          key={item}
+          className="text-gray-400 text-sm hover:underline cursor-pointer"
+        >
+          {item}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default List;
