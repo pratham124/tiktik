bb9448434bc7b83a849e567ae26ff86886e115dc
Pratham Sitoula <psitoula2003@gmail.com>
2022-08-17 13:25:43 -0600

    Video section of detail page implemented

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -27,6 +27,12 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
     }
   };
 
+  useEffect(() => {
+    if (videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [isVideoMuted]);
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -76,6 +82,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               src={post.video.asset.url}
               className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
               ref={videoRef}
+              loop
             ></video>
           </Link>
 
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from "axios";
+import { BASE_URL } from "../../utils";
+import { Video } from "../../types";
 
-const Detail = () => {
-  return (
-    <div>[id]</div>
-  )
+interface Iprops {
+  postDetails: Video;
 }
 
-export default Detail
+const Detail = ({ postDetails }: Iprops) => {
+  const [post, setpost] = useState(postDetails);
+  const [playing, setplaying] = useState(false);
+  const [isVideoMuted, setisVideoMuted] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [isVideoMuted]);
+
+  if (!post) return null;
+
+  const onVideoClickHandler = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setplaying(false);
+    } else {
+      videoRef?.current?.play();
+      setplaying(true);
+    }
+  };
+
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center  bg-black">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p className="cursor-pointer" onClick={() => router.back()}>
+            <MdOutlineCancel className="text-white text-[35px] cursor-pointer" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+              ref={videoRef}
+              loop
+              onClick={onVideoClickHandler}
+            ></video>
+          </div>
+
+          <div className="absolute top-[45%] left-[45%] cursor-pointer">
+            {!playing && (
+              <button onClick={onVideoClickHandler}>
+                <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+              </button>
+            )}
+          </div>
+        </div>
+
+        <div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer">
+          {isVideoMuted && (
+            <button
+              onClick={() => {
+                setisVideoMuted(false);
+              }}
+            >
+              <HiVolumeOff className="text-white text-2xl lg:text-4xl" />
+            </button>
+          )}
+          {!isVideoMuted && (
+            <button
+              onClick={() => {
+                setisVideoMuted(true);
+              }}
+            >
+              <HiVolumeUp className="text-white text-2xl lg:text-4xl" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: {
+      postDetails: data,
+    },
+  };
+};
